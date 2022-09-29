13223064e93e8312f1e5f30b1e67657977410e88
nsailaja436@gmail.com <nsailaja436@gmail.com>
2022-09-29 12:24:13 +0200

    ResponsiveGrid_For AlltypeofDevices

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import { GridPage } from './GridPage';
+import lines from './Images/button.png';
+import { useState } from 'react';
+
+var stylingObject={
+  div:{'scroll-behavior':'smooth'}
+}
 
 function App() {
+  const [panelhide,setPanelhide]=useState(false);
+  const[modClick,setModClick]=useState(true);
+  const moduleSelected=()=>{
+      setModClick(()=>false)
+      
+  }
+  const fnPanel=()=>{
+    setPanelhide(()=>!panelhide);
+    setModClick(()=>true)
+  }
+  
   return (
-    <div className="App" >
-      <div style={{fontWeight:'bold',fontFamily:'sans-serif'}}>Sample Application</div>
-      <GridPage/>
+    <div>
+      <div id='scrolltop' className="container-fluid" >
+        <div className="row" style={{backgroundColor:'green',height:'25px',position:'sticky',top:0}}>
+          <div className="col-1" onClick={fnPanel}> 
+            <img src={lines} height={20} width={20} />
+          </div>
+          <div className='col-11 text-center' style={{fontWeight:'bold',fontFamily:'sans-serif',color:'white'}}>
+            <span style={{display:'inline-block'}}>Sample Application</span>
+          </div>
+        </div>
+        <GridPage phide={panelhide} modhide={modClick} modhidefun={moduleSelected}/>
+
+      </div>
+      <div style={{fontWeight:'bold',color:'burlywood',position:'fixed',bottom:0,cursor:'pointer',scrollBehavior:'smooth'}}><a href='#scrolltop'>Scroll Top <span style={{fontSize:'50px'}}>&#8593;</span></a></div>
+    
     </div>
+    
   );
 }
 
