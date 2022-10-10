07c6811d7b7d66f0adcf983203282aaac3618f1a
nsailaja436@gmail.com <nsailaja436@gmail.com>
2022-10-10 22:10:51 +0200

    Menu Panel Images _ Links Names are changed

--- src/GridPage.jsx
+++ src/GridPage.jsx
@@ -26,13 +26,13 @@ export const GridPage=({phide,modhide,modhidefun})=>{
                         {modhide?
                             <div className="row" style={{border:'0px solid cyan',backgroundColor:'#E1FFB1',position:'fixed',top:30}}>Mobile Navigation
                                 <div className="row" onClick={moduleSelected} style={{border:'0px solid cyan'}}> 
-                                    <NavButton><NavLink to='/module1'><div className="col-sm-2">Pending</div></NavLink></NavButton>
-                                    <NavButton><NavLink to='/module2'><div className="col-sm-2">Completed</div></NavLink></NavButton>
+                                    <NavButton><NavLink to='/create-datacard'><div className="col-sm-2">Create Datacard</div></NavLink></NavButton>
+                                    <NavButton><NavLink to='/datacard-list'><div className="col-sm-2">Datacard List</div></NavLink></NavButton>
                                     <NavButton><NavLink to='/module3'><div className="col-sm-2">Cancelled</div></NavLink></NavButton>
                                     <NavButton><NavLink to='/module4'><div className="col-sm-2">Search by supplier</div></NavLink></NavButton>
                                     <NavButton><NavLink to='/module5'><div className="col-sm-2">Search by E-id</div></NavLink></NavButton>
                                     <NavButton><NavLink to='/module6'><div className="col-sm-2">Module6</div></NavLink></NavButton>
-                                    <NavButton><NavLink to='/module7'><div className="col-sm-2">Module7</div></NavLink></NavButton>
+                                    <NavButton><NavLink to='/game'><div className="col-sm-2">Game</div></NavLink></NavButton>
                                 </div>
                             </div>:''
                         }
@@ -45,13 +45,13 @@ export const GridPage=({phide,modhide,modhidefun})=>{
                                         <div className="d-none d-md-block col-xs-1 col-sm-1 col-md-1 col-lg-1"><HidePanelShortcuts/></div>
                                         <div className="d-none d-md-block col-xs-10 col-sm-10 col-md-10 col-lg-10">
                                         
-                                            <div><NavButton><NavLink to='/module1' ><div className="col-sm-10 col-md-10 col-lg-10">Pending</div></NavLink></NavButton></div>
-                                            <div><NavButton><NavLink to='/module2' ><div className="col-sm-10 col-md-10 col-lg-10">Completed</div></NavLink></NavButton></div>
+                                            <div><NavButton><NavLink to='/create-datacard' ><div className="col-sm-10 col-md-10 col-lg-10">Create Datacard</div></NavLink></NavButton></div>
+                                            <div><NavButton><NavLink to='/datacard-list' ><div className="col-sm-10 col-md-10 col-lg-10">Datacard List</div></NavLink></NavButton></div>
                                             <div><NavButton><NavLink to='/module3' ><div className="col-sm-10 col-md-10 col-lg-10">Cancelled</div></NavLink></NavButton></div>
                                             <div><NavButton><NavLink to='/module4' ><div className="col-sm-10 col-md-10 col-lg-10">Search by supplier</div></NavLink></NavButton></div>
                                             <div><NavButton><NavLink to='/module5' ><div className="col-sm-10 col-md-10 col-lg-10">Search by employer id</div></NavLink></NavButton></div>
                                             <div><NavButton><NavLink to='/module6' ><div className="col-sm-10 col-md-10 col-lg-10">Module6</div></NavLink></NavButton></div>
-                                            <div><NavButton><NavLink to='/module7' ><div className="col-sm-10 col-md-10 col-lg-10">Module7</div></NavLink></NavButton></div>
+                                            <div><NavButton><NavLink to='/game' ><div className="col-sm-10 col-md-10 col-lg-10">Game</div></NavLink></NavButton></div>
                                         </div>
                                     </div>
                                 </div>
@@ -75,13 +75,13 @@ export const GridPage=({phide,modhide,modhidefun})=>{
                                 <div className="row">
                                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center" style={{border:'0px dashed red'}}>
                                         <Routes>
-                                            <Route path='/module1' element={<Module1/>}/>
-                                            <Route path='/module2' element={<Module2/>}/>
+                                            <Route path='/create-datacard' element={<Module1/>}/>
+                                            <Route path='/datacard-list' element={<Module2/>}/>
                                             <Route path='/module3' element={<Module3/>}/>
                                             <Route path='/module4' element={<Module4/>}/>
                                             <Route path='/module5' element={<Module5/>}/>
                                             <Route path='/module6' element={<Module6/>}/>
-                                            <Route path='/module7' element={<Module7/>}/>
+                                            <Route path='/game' element={<Module7/>}/>
                                         </Routes>
                                     </div>
                                 </div>
@@ -90,13 +90,13 @@ export const GridPage=({phide,modhide,modhidefun})=>{
                     </div>:
                     <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10 text-center" style={{border:'0px solid green'}}>
                         <Routes>
-                            <Route path='/module1' element={<Module1/>}/>
-                            <Route path='/module2' element={<Module2/>}/>
+                            <Route path='/create-datacard' element={<Module1/>}/>
+                            <Route path='/datacard-list' element={<Module2/>}/>
                             <Route path='/module3' element={<Module3/>}/>
                             <Route path='/module4' element={<Module4/>}/>
                             <Route path='/module5' element={<Module5/>}/>
                             <Route path='/module6' element={<Module6/>}/>
-                            <Route path='/module7' element={<Module7/>}/>
+                            <Route path='/game' element={<Module7/>}/>
                         </Routes>
                     </div>
                     
